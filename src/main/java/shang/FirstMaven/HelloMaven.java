61148e008d17154047841d8482bb63cf7b9eac8c
StoneShiShang <43474457+Stoneshishang@users.noreply.github.com>
2021-01-16 17:28:59 -0500

    add logback.xml configuration file to config the log output format

--- src/main/java/shang/FirstMaven/HelloMaven.java
+++ src/main/java/shang/FirstMaven/HelloMaven.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 public class HelloMaven {
 
     private final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+    
     public static void main(String[] args) {
         log.info("Hello Info");
         log.debug("Hello Debug");
